7406e10e12a9921e27e3c3e2c248beced9a89e9b
myeong-jae-hwi <audwognl@gmail.com>
2025-02-11 09:21:02 +0900

    [구성]: 뒤로가기 구현

--- test/src/playground.tsx
+++ test/src/playground.tsx
@@ -6,10 +6,11 @@ import Router from '@/router';
 
 function Playground() {
   const [route, setRoute] = useState(getView);
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
     const handlePopState = () => {
-      const params = new URLSearchParams(location.search);
+      const params = new URLSearchParams(location.search); // webAPI React 관리 X
       const nextRoute = params.get('view');
       console.log(params.get('view'));
 
@@ -17,8 +18,20 @@ function Playground() {
         setRoute(nextRoute);
       }
     };
-    window.addEventListener('popstate', handlePopState);
-  }, []);
+    window.addEventListener('popstate', handlePopState); // 마운트 되었을 때, count가 변경되었을 때
+    console.log('update');
+
+    return () => {
+      // clean up
+      window.removeEventListener('popstate', handlePopState);
+      console.log('clean up');
+    };
+  }, [count]);
+
+  const handleCount = () => {
+    setCount(count + 1);
+    console.log(count);
+  };
 
   return (
     <section className="Playground bg-euid-gray-200 wrapper">
@@ -26,6 +39,14 @@ function Playground() {
         <ReactPingIcon size={24} /> Playground
       </h1>
       <Nav onChangeRoute={setRoute} />
+      <button
+        type="button"
+        className="mt-4 px-4 py-2 bg-react text-white rounded-md"
+        onClick={handleCount}
+      >
+        땡쥬
+      </button>
+
       <Router route={route} />
     </section>
   );
