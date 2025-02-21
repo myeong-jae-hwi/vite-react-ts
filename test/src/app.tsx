6aca73779e0e08e3c87a9e0ad1af61131ddcba62
myeong-jae-hwi <audwognl@gmail.com>
2025-02-21 13:37:50 +0900

    [변경]: react-router 추가 및 Playground 컴포넌트 라우팅 구현

--- test/src/app.tsx
+++ test/src/app.tsx
@@ -1,14 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router';
 import { ErrorBoundary } from 'react-error-boundary';
-import PrintError from './components/error';
-import Playground from './playground';
 import { ThemeProvider, ThemeSetters } from './contexts/theme';
+import Playground from './pages/playground/page';
+import PrintError from './components/error';
 
 function App() {
+  
   return (
     <ErrorBoundary FallbackComponent={PrintError}>
       <ThemeProvider>
         <ThemeSetters />
-        <Playground />
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
+          <Routes>
+            <Route path="/playground" element={<Playground />} />
+          </Routes>
+        </BrowserRouter>
       </ThemeProvider>
     </ErrorBoundary>
   );
